d74f6c8efc6cc90f8d383f8e85f43216df0693d4
James23Wang <365355320@qq.com>
2020-06-12 17:57:15 +0100

    添加文件

---
+++ HelloGit/src/main/java/com/james/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.james.git;
+
+/**
+ * @author Zhe Wang
+ * @version 2020-06-12
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
